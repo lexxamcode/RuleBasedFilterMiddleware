ea9b375cfc77eaa3d9835ca6c931464cedf56531
lexxamcode <lexxamcode@mail.ru>
2025-02-11 21:02:55 +0400

    service registration change at Program.cs

--- TestTileApi/Program.cs
+++ TestTileApi/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddRuleBasedRequestFilterServices();
+builder.Services.AddRuleBasedRequestFilterServices(RuleBasedRequestFilterOptions.Default);
 
 var tilesDirectory = builder.Configuration["Tiles:LocalStoragePath"] ?? string.Empty;
 
